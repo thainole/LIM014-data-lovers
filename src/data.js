ca7578b505d2734e9161c7d302b44d460ccfe1fd
thainole <thainole@gmail.com>
2021-02-08 11:33:14 -0500

    show athletes in cards

--- src/data.js
+++ src/data.js
@@ -1,14 +1,36 @@
-//métodos 
+// Filter
 export const filterTeam = (data, condition) => {
   return data.filter(athletes => athletes.team.includes(condition));
-  //   return data.filter(athletes => athletes.team == condition);
-
 };
 
 export const filterSport = (data, condition) => {
   return data.filter(athletes => athletes.sport.includes(condition));
 };
 
-export const mapTeams = (data) => {
+
+//Sort 
+export const sortByName = (data) => {
+  return data.sort((a,b) => (a.name > b.name ? 1 : -1));
+};
+
+
+//Map
+export const mapTeam = (data) => {
   return data.map(athletes => athletes.team);
 };
+
+export const mapSport = (data) => {
+  return data.map(athletes => athletes.sport);
+}
+
+
+/*export const sortByName = (data, sortData) => {
+  const sortResult = data.sort((a,b) => (a.name > b.name ? 1 : -1));
+  if (sortData == "A-Z") {
+    return sortResult;
+  } else {
+    return sortResult.reverse();
+  }
+};*/
+
+
--- src/main.js
+++ src/main.js
@@ -1,49 +1,59 @@
-import { filterTeam, filterSport, mapTeams } from './data.js';
-//cuando agregue una función, tengo que incluirla aquí uwu
+import { filterTeam, filterSport, mapTeam, mapSport, sortByName} from './data.js';
 import data from './data/athletes/athletes.js';
 
 const athletesData = data.athletes;
 
-// let card = document.querySelectorAll(".card");
-  /*
-function showAthletes (data) {
 
+// Lista de países en orden alfabético
+const repeatedTeams = mapTeam(athletesData);
+const teams = [...new Set(repeatedTeams)].sort((a,b) => a > b ? 1 : -1);
+console.log(teams);
+
+
+// Lista de deportes en orden alfabético
+const repeatedSports = mapSport(athletesData);
+const sports = [...new Set(repeatedSports)].sort((a,b) => a > b ? 1 : -1);
+console.log(sports);
+
+
+// Mostrar atletas en las tarjetas
+const resultsPage = document.querySelector(".page-search__main__results-area__grid");
+
+const showAthletes = (data) => {
+    resultsPage.innerHTML="";
     let counter = 0;
-    data.forEach(athletes => {
+    data.forEach((athletes) => {
         counter++;
-        if (counter <= 9) {
-            card.innerHTML += `
+        if (counter <= 30) {
+            resultsPage.innerHTML += `
+            <div class="card">
             <div class="card__title">
-            ${athletes.name}
+                ${athletes.name}
             </div>
             <ul class="card__content">
-            <li>Sport: ${athletes.sport}</li>
-            <li>Team: ${athletes.team}</li>
-            <li>Gender: ${athletes.gender}</li>
+                <li>Gender: ${athletes.gender}</li>
+                <li>Team: ${athletes.team}</li>
+                <li>Sport: ${athletes.sport}</li>
             </ul>
-            `;
-        }
+            </div>`;
+        };
     });
     return showAthletes;
-} 
+}
 showAthletes(athletesData);
 
+
+// Consola para verificar que funcionen los filtros
 console.log(filterTeam(athletesData,"Italy"));
 console.log(filterSport(athletesData, "Handball"));
-
-// Lista de países 
-const repeatedTeams = mapTeams(athletesData);
-const teams = [...new Set(repeatedTeams)];
-console.log(teams);
-//const selectTeam = document.querySelector("#team").value;*/
-
+console.log(sortByName(athletesData));
 
 
 // FUNCIONALIDAD BOTONES DE LA BARRA DE NAVEGACIÓN
 const homeButton = document.getElementById("home-button");
 const champsButton = document.getElementById("champs-button");
 const anotherChampsButton = document.getElementById("another-champs-button");
-// const statsButton = document.getElementById("stats-button");
+/* const statsButton = document.getElementById("stats-button"); */
 
 homeButton.addEventListener("click", homePage);
 function homePage () {
