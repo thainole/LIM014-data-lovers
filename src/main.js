d1122e2172c88eef3e8066a4920e5d9eaeb17b89
thainole <thainole@gmail.com>
2021-02-09 10:45:09 -0500

    create pop up

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterTeam, filterSport, mapTeam, mapSport, sortByName} from './data.js';
+import { filterTeam, filterSport, filterGender, mapTeam, mapSport, sortByName} from './data.js';
 import data from './data/athletes/athletes.js';
 
 const athletesData = data.athletes;
@@ -9,6 +9,9 @@ const repeatedTeams = mapTeam(athletesData);
 const teams = [...new Set(repeatedTeams)].sort((a,b) => a > b ? 1 : -1);
 console.log(teams);
 
+// Poner los países en el botón de select
+const selectTeam = document.getElementById("select-team");
+
 
 // Lista de deportes en orden alfabético
 const repeatedSports = mapSport(athletesData);
@@ -16,15 +19,17 @@ const sports = [...new Set(repeatedSports)].sort((a,b) => a > b ? 1 : -1);
 console.log(sports);
 
 
-// Mostrar atletas en las tarjetas
-const resultsPage = document.querySelector(".page-search__main__results-area__grid");
 
-const showAthletes = (data) => {
+
+// Mostrar atletas en las tarjetas
+let resultsPage = document.querySelector(".page-search__main__results-area__grid");
+
+let showAthletes = (data) => {
     resultsPage.innerHTML="";
     let counter = 0;
     data.forEach((athletes) => {
         counter++;
-        if (counter <= 30) {
+        if (counter <= 40) {
             resultsPage.innerHTML += `
             <div class="card">
             <div class="card__title">
@@ -35,6 +40,9 @@ const showAthletes = (data) => {
                 <li>Team: ${athletes.team}</li>
                 <li>Sport: ${athletes.sport}</li>
             </ul>
+            <div class="card__read-more">
+                <button>Read more</button>
+            </div>
             </div>`;
         };
     });
@@ -42,11 +50,64 @@ const showAthletes = (data) => {
 }
 showAthletes(athletesData);
 
+//Ver info completa del participante
+const readMore = document.querySelector(".card__read-more");
+let popUp = document.querySelector(".page-search__pop-up-wrapper");
+readMore.addEventListener("click", () => {
+    popUp.style.display = "block"
+});
+const popUpClose = document.querySelector(".page-search__pop-up-close");
+popUpClose.addEventListener("click", () => {
+    popUp.style.display = "none";
+})
+popUp.addEventListener("click", e => {
+    if (e.target.className === "page-search__pop-up-wrapper") {
+        popUp.style.display = "none";
+    }
+}) 
+let popUpContent = document.querySelector(".page-search__pop-up-content");
+let showAthletesFullData = (data) => {
+    let counter = 0;
+    data.forEach((athletes) => {
+        counter++;
+        if (counter <= 40) {
+            popUpContent.innerHTML=`
+            <div class="card__title">${athletes.name}</div>
+            <div class="card__content">
+                <li>Gender: ${athletes.gender}</li>
+                <li>Height: ${athletes.height}</li>
+                <li>Weight: ${athletes.weight}</li>
+                <li>Sport: ${athletes.sport}</li>
+                <li>Team: ${athletes.team}</li>
+                <li>Noc: ${athletes.noc}</li>
+                <li>Age: ${athletes.age}</li>
+                <li>Event: ${athletes.event}</li>
+                <li>Medal: ${athletes.medal}</li>        
+            </div>    
+        `;
+        };
+    });
+    return showAthletesFullData;
+}
+showAthletesFullData(athletesData)
+
+
+
+// Ordenar de la A-Z 
+const az = document.getElementById("a-z");
+const za = document.getElementById("z-a");
+
+
+// Filtrar por género 
+console.log(filterGender(athletesData, "F"));
+console.log(filterGender(athletesData, "M"));
+
 
 // Consola para verificar que funcionen los filtros
 console.log(filterTeam(athletesData,"Italy"));
 console.log(filterSport(athletesData, "Handball"));
-console.log(sortByName(athletesData));
+//console.log(sortByName(athletesData));
+
 
 
 // FUNCIONALIDAD BOTONES DE LA BARRA DE NAVEGACIÓN
@@ -67,4 +128,3 @@ function champsPage () {
     document.querySelector(".page-search").style.display="block"
     document.querySelector(".home-main").style.display="none";
 }
-
