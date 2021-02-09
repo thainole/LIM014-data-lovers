bf9d792b4a2cc06de772ba7fbe2e38c65a5d96d1
thainole <thainole@gmail.com>
2021-02-09 12:32:03 -0500

    show cards and pop-ups with same data

--- src/main.js
+++ src/main.js
@@ -1,37 +1,29 @@
-import { filterTeam, filterSport, filterGender, mapTeam, mapSport, sortByName} from './data.js';
+import { filterTeam, filterSport, filterGender, mapTeam, mapSport, sortByName } from './data.js';
 import data from './data/athletes/athletes.js';
 
+// Declarando variables
 const athletesData = data.athletes;
+let popUp = document.querySelector(".page-search__pop-up-wrapper");
+let popUpContent = document.querySelector(".page-search__pop-up-content");
+const popUpClose = document.querySelector(".page-search__pop-up-close");
+const homeButton = document.getElementById("home-button");
+const champsButton = document.getElementById("champs-button");
+const anotherChampsButton = document.getElementById("another-champs-button");
+/* const statsButton = document.getElementById("stats-button"); */
 
 
-// Lista de países en orden alfabético
-const repeatedTeams = mapTeam(athletesData);
-const teams = [...new Set(repeatedTeams)].sort((a,b) => a > b ? 1 : -1);
-console.log(teams);
-
-// Poner los países en el botón de select
-const selectTeam = document.getElementById("select-team");
-
-
-// Lista de deportes en orden alfabético
-const repeatedSports = mapSport(athletesData);
-const sports = [...new Set(repeatedSports)].sort((a,b) => a > b ? 1 : -1);
-console.log(sports);
-
-
-
-
-// Mostrar atletas en las tarjetas
+// Mostrar atletas en las tarjetas y pop up
 let resultsPage = document.querySelector(".page-search__main__results-area__grid");
 
 let showAthletes = (data) => {
-    resultsPage.innerHTML="";
+    resultsPage.innerHTML = "";
     let counter = 0;
     data.forEach((athletes) => {
         counter++;
         if (counter <= 40) {
-            resultsPage.innerHTML += `
-            <div class="card">
+            const div = document.createElement("div");
+            div.classList.add("card");
+            div.innerHTML = `
             <div class="card__title">
                 ${athletes.name}
             </div>
@@ -40,23 +32,22 @@ let showAthletes = (data) => {
                 <li>Team: ${athletes.team}</li>
                 <li>Sport: ${athletes.sport}</li>
             </ul>
-            <div class="card__read-more">
+            <div class="card__read-more flex-center">
                 <button>Read more</button>
-            </div>
             </div>`;
+            resultsPage.appendChild(div)
+            const readMore = div.querySelector(".card__read-more");
+
+            readMore.addEventListener("click", () => {
+                popUp.style.display = "block";
+                popUpContent.innerHTML =showAthletesFullData(athletes);
+            });
         };
     });
     return showAthletes;
 }
 showAthletes(athletesData);
 
-//Ver info completa del participante
-const readMore = document.querySelector(".card__read-more");
-let popUp = document.querySelector(".page-search__pop-up-wrapper");
-readMore.addEventListener("click", () => {
-    popUp.style.display = "block"
-});
-const popUpClose = document.querySelector(".page-search__pop-up-close");
 popUpClose.addEventListener("click", () => {
     popUp.style.display = "none";
 })
@@ -64,14 +55,10 @@ popUp.addEventListener("click", e => {
     if (e.target.className === "page-search__pop-up-wrapper") {
         popUp.style.display = "none";
     }
-}) 
-let popUpContent = document.querySelector(".page-search__pop-up-content");
-let showAthletesFullData = (data) => {
-    let counter = 0;
-    data.forEach((athletes) => {
-        counter++;
-        if (counter <= 40) {
-            popUpContent.innerHTML=`
+})
+
+let showAthletesFullData = (athletes) => {
+    let content = `
             <div class="card__title">${athletes.name}</div>
             <div class="card__content">
                 <li>Gender: ${athletes.gender}</li>
@@ -83,48 +70,41 @@ let showAthletesFullData = (data) => {
                 <li>Age: ${athletes.age}</li>
                 <li>Event: ${athletes.event}</li>
                 <li>Medal: ${athletes.medal}</li>        
-            </div>    
-        `;
-        };
-    });
-    return showAthletesFullData;
-}
-showAthletesFullData(athletesData)
+            </div>`;
+    return content;
+};
 
 
+// Lista de países en orden alfabético
+const repeatedTeams = mapTeam(athletesData);
+const teams = [...new Set(repeatedTeams)].sort((a, b) => a > b ? 1 : -1);
+console.log(teams);
 
-// Ordenar de la A-Z 
-const az = document.getElementById("a-z");
-const za = document.getElementById("z-a");
+
+// Lista de deportes en orden alfabético
+const repeatedSports = mapSport(athletesData);
+const sports = [...new Set(repeatedSports)].sort((a, b) => a > b ? 1 : -1);
+console.log(sports);
 
 
-// Filtrar por género 
+// Consola para verificar que funcionen los filtros
+console.log(filterTeam(athletesData, "Italy"));
+console.log(filterSport(athletesData, "Handball"));
+console.log(sortByName(athletesData));
 console.log(filterGender(athletesData, "F"));
 console.log(filterGender(athletesData, "M"));
 
 
-// Consola para verificar que funcionen los filtros
-console.log(filterTeam(athletesData,"Italy"));
-console.log(filterSport(athletesData, "Handball"));
-//console.log(sortByName(athletesData));
-
-
-
-// FUNCIONALIDAD BOTONES DE LA BARRA DE NAVEGACIÓN
-const homeButton = document.getElementById("home-button");
-const champsButton = document.getElementById("champs-button");
-const anotherChampsButton = document.getElementById("another-champs-button");
-/* const statsButton = document.getElementById("stats-button"); */
-
+// Fincionalidad de la barra de navegación
 homeButton.addEventListener("click", homePage);
-function homePage () {
-    document.querySelector(".home-main").style.display="block";
-    document.querySelector(".page-search").style.display="none";
+function homePage() {
+    document.querySelector(".home-main").style.display = "block";
+    document.querySelector(".page-search").style.display = "none";
 }
 
 anotherChampsButton.addEventListener("click", champsPage);
 champsButton.addEventListener("click", champsPage);
-function champsPage () {
-    document.querySelector(".page-search").style.display="block"
-    document.querySelector(".home-main").style.display="none";
+function champsPage() {
+    document.querySelector(".page-search").style.display = "block"
+    document.querySelector(".home-main").style.display = "none";
 }
