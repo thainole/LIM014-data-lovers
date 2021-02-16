50cfaad7c244b39edde8558425e8c802ad7022ae
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-02-16 16:30:14 -0500

    women button applying filter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { sortByAz, sortByZa, filterName, filterTeam, filterSport, filterEvent, mapTeam, mapSport, mapEvent, filterFemale, filterMale } from './data.js';
-import data from './data/athletes/athletes.js'; 
+import { sortByAz, sortByZa, filterName, filterTeam, filterSport, filterEvent, mapTeam, mapSport, mapEvent, mapFemale, filterFemale, filterMale } from './data.js';
+import data from './data/athletes/athletes.js';
 
 
 // Declarando variables
@@ -12,6 +12,7 @@ const homeButton = document.getElementById("home-button");
 const statsButton = document.getElementById("stats-button");
 const champsButton = document.getElementById("champs-button");
 const anotherChampsButton = document.getElementById("another-champs-button");
+const womenButton = document.getElementById("womenButton");
 
 const repeatedTeams = mapTeam(athletesData);
 const repeatedSports = mapSport(athletesData);
@@ -196,7 +197,9 @@ selectEvent.addEventListener("change", () => {
 });
 //gender selection
 //gender: female
-selectFemale.addEventListener("change", () => {
+selectFemale.addEventListener("change", selectFemaleFunction);
+
+function selectFemaleFunction() {
     const searchStringSport = selectSport.value.toLowerCase(); //
     let filteredNames = filterSport(athletesData, searchStringSport);
 
@@ -221,7 +224,7 @@ selectFemale.addEventListener("change", () => {
         resultsPage.innerHTML = "";
         showAthletes(filteredNames);
     }
-});
+};
 //gender: male
 selectMale.addEventListener("change", () => {
     const searchStringSport = selectSport.value.toLowerCase(); //
@@ -291,10 +294,27 @@ function homePage() {
 anotherChampsButton.addEventListener("click", champsPage);
 champsButton.addEventListener("click", champsPage);
 
+//women button
+womenButton.addEventListener("click", womenPage);
+
+function womenPage() {
+    let women = filterFemale(athletesData)
+
+    console.log("click");
+    document.querySelector(".page-search").style.display = "block";
+    document.querySelector(".home-main").style.display = "none";
+    resultsPage.innerHTML = ''
+    showAthletes(women);
+
+}
+//
+
+
 function champsPage() {
     console.log("click");
     document.querySelector(".page-search").style.display = "block";
     document.querySelector(".home-main").style.display = "none";
+    resultsPage.innerHTML = ''
     showAthletes(athletesData);
 }
 
