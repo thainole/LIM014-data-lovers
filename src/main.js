e5dc6cdfe5b912af8e159ef0578d8b9d2c5510c1
thainole <thainole@gmail.com>
2021-02-24 16:30:05 -0500

    burger menu finished

--- src/main.js
+++ src/main.js
@@ -248,3 +248,24 @@ function statsPage() {
     document.querySelector(".page-search").style.display = "none";
     document.querySelector(".home-main").style.display = "none";
 }
+
+const navSlide = () => {
+    const burger = document.querySelector('.nav__mobile');
+    const nav = document.querySelector('.nav__links');
+    const navLinks = document.querySelectorAll('.nav__links li');
+ 
+    burger.addEventListener('click', ()=>{
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+    
+        //Animate Links
+        navLinks.forEach((link, index)=>{
+            if (link.style.animation) {
+                link.style.animation ='';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
+            }
+        });
+    });
+}
+navSlide();
